95804b21f210c5f6cda3ee318d77e88353220d2b
Matias Insaurralde <matias@insaurral.de>
2016-11-26 04:31:05 -0300

    Add helper for key parsing.

--- crypto/keys.go
+++ crypto/keys.go
@@ -6,6 +6,7 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"crypto/x509"
+	"log"
 
 	"golang.org/x/crypto/ripemd160"
 
@@ -29,4 +30,21 @@ func GenerateKeys() (privateKeyBytes []byte, publicKeyBytes []byte, peerID strin
 	peerID = base58check.Encode("00", hash)
 
 	return privateKeyBytes, publicKeyBytes, peerID
+}
+
+// ParseKeys will parse existing key buffers and return the appropiate data structures.
+func ParseKeys(privateKeyBytes []byte, publicKeyBytes []byte) (privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey, err error) {
+	privateKey, err = x509.ParseECPrivateKey(privateKeyBytes)
+	if err != nil {
+		log.Println("Couldn't parse private key!")
+		return nil, nil, err
+	}
+	var pub interface{}
+	pub, err = x509.ParsePKIXPublicKey(publicKeyBytes)
+	if err != nil {
+		log.Println("Couldn't parse public key!")
+		return nil, nil, err
+	}
+	publicKey = pub.(*ecdsa.PublicKey)
+	return privateKey, publicKey, err
 }
