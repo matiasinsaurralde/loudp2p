d8c8c09e627452e260413ebf1a4ad578b2c4d265
Matias Insaurralde <matias@insaurral.de>
2016-11-26 07:58:28 -0300

    Modify peer data structure to hold gRPC client.

--- core/peer.go
+++ core/peer.go
@@ -1,6 +1,13 @@
 package loudp2p
 
+import (
+	loud "github.com/matiasinsaurralde/loudp2p/pb"
+	"google.golang.org/grpc"
+)
+
 // Peer represents the standard data structure for peers.
 type Peer struct {
 	Address string
+	Conn    *grpc.ClientConn
+	Client  loud.LoudClient
 }
