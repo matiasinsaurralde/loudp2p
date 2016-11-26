d49fdc54bbc7680fb7f8ceeccbe05c36c685d1d2
Matias Insaurralde <matias@insaurral.de>
2016-11-25 22:34:02 -0300

    Refactor GenerateKeys.

--- crypto/keys.go
+++ crypto/keys.go
@@ -1,31 +1,32 @@
 package crypto
 
-import(
-  "crypto/elliptic"
-  "crypto/rand"
-  "crypto/ecdsa"
-  "crypto/x509"
-  "crypto/sha256"
-  "golang.org/x/crypto/ripemd160"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/x509"
 
-  "github.com/matiasinsaurralde/hellobitcoin/base58check"
+	"golang.org/x/crypto/ripemd160"
+
+	"github.com/matiasinsaurralde/hellobitcoin/base58check"
 )
 
-func GenerateKeys() ([]byte, []byte, string) {
-  var private_key_bytes, public_key_bytes []byte
-  private_key, _ := ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
-	private_key_bytes, _ = x509.MarshalECPrivateKey(private_key)
-	public_key_bytes, _ = x509.MarshalPKIXPublicKey(&private_key.PublicKey)
+// GenerateKeys will generate the peer initial keys and ID.
+func GenerateKeys() (privateKeyBytes []byte, publicKeyBytes []byte, peerID string) {
+	privateKey, _ := ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
+	privateKeyBytes, _ = x509.MarshalECPrivateKey(privateKey)
+	publicKeyBytes, _ = x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
 
-  shaHash := sha256.New()
-  shaHash.Write(public_key_bytes)
-  hash := shaHash.Sum(nil)
+	shaHash := sha256.New()
+	shaHash.Write(publicKeyBytes)
+	hash := shaHash.Sum(nil)
 
-  ripemd160Hash := ripemd160.New()
-  ripemd160Hash.Write(hash)
-  hash = ripemd160Hash.Sum(nil)
+	ripemd160Hash := ripemd160.New()
+	ripemd160Hash.Write(hash)
+	hash = ripemd160Hash.Sum(nil)
 
-  b58 := base58check.Encode("00", hash)
+	peerID = base58check.Encode("00", hash)
 
-  return private_key_bytes, public_key_bytes, b58
+	return privateKeyBytes, publicKeyBytes, peerID
 }
