8b7573da9722cf8b9ccc528379217ab2b84a5eff
Matias Insaurralde <matias@insaurral.de>
2016-11-26 07:58:59 -0300

    Initial client implementation.

--- core/client.go
+++ core/client.go
@@ -1,26 +1,30 @@
 package loudp2p
 
 import (
+	"context"
 	"errors"
 	"log"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/matiasinsaurralde/loudp2p/pb"
 )
 
 var (
 	initialPeerList = []string{
 		"127.0.0.1:5555",
-		"127.0.0.1:5556",
 	}
+	dialOpts []grpc.DialOption
 )
 
 // Client represents a standard data structure for clients.
 type Client struct {
-	Peers []Peer
+	Peers    []Peer
+	Settings *Settings
 }
 
 // NewClient initializes a new client, using the data from settings.
 func NewClient(settings *Settings) (client Client, err error) {
-	// log.Println("Starting client.")
-
 	if settings == nil {
 		err = errors.New("Invalid settings")
 		return client, err
@@ -31,12 +35,18 @@ func NewClient(settings *Settings) (client Client, err error) {
 		return client, err
 	}
 
+	// Set global gRPC connection options:
+	dialOpts = append(dialOpts, grpc.WithInsecure())
+
 	client = Client{
-		Peers: make([]Peer, 0),
+		Peers:    make([]Peer, 0),
+		Settings: settings,
 	}
 
 	for _, peerAddr := range initialPeerList {
-		peer := Peer{peerAddr}
+		peer := Peer{
+			Address: peerAddr,
+		}
 		client.Peers = append(client.Peers, peer)
 	}
 
@@ -48,11 +58,24 @@ func (c *Client) Start() {
 	log.Println("Discovery starts.")
 
 	for _, peer := range c.Peers {
-		c.AnnounceTo(peer)
+		go c.AnnounceTo(peer)
 	}
 }
 
 // AnnounceTo is used to announce a client to another peer.
 func (c *Client) AnnounceTo(peer Peer) {
 	log.Println("Announcing to:", peer)
+	var err error
+	peer.Conn, err = grpc.Dial(peer.Address, dialOpts...)
+	if err != nil {
+		log.Println(peer, "couldn't establish connection!")
+		return
+	}
+
+	peer.Client = pb.NewLoudClient(peer.Conn)
+	helloMessage := pb.HelloMessage{
+		Origin: c.Settings.PeerID,
+	}
+
+	peer.Client.Hello(context.Background(), &helloMessage)
 }
