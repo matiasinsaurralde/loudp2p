41adbb122ea27ad73b619e9e92441aeb094ad6e1
Matias Insaurralde <matias@insaurral.de>
2016-11-27 11:35:14 -0300

    Fix tests.

--- core/client_test.go
+++ core/client_test.go
@@ -16,17 +16,21 @@ var (
 	testPrivKey     *ecdsa.PrivateKey
 	privateKeyBytes []byte
 	publicKeyBytes  []byte
+
+	testEventHandler EventHandler
 )
 
 func init() {
 	testPrivKey, _ := ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
 	privateKeyBytes, _ = x509.MarshalECPrivateKey(testPrivKey)
 	publicKeyBytes, _ = x509.MarshalPKIXPublicKey(&testPrivKey.PublicKey)
+
+	testEventHandler = NewEventHandler()
 }
 
 func TestClientInitialization(t *testing.T) {
 	var err error
-	_, err = NewClient(nil)
+	_, err = NewClient(nil, &testEventHandler)
 
 	if err == nil {
 		t.Fatal("A client with no settings must fail to initialize.")
@@ -34,32 +38,32 @@ func TestClientInitialization(t *testing.T) {
 
 	var settings Settings
 	settings = Settings{}
-	_, err = NewClient(&settings)
+	_, err = NewClient(&settings, &testEventHandler)
 	if err == nil {
 		t.Fatal("A client with an empty settings data structure must fail to initialize.")
 	}
 
 	settings = Settings{PrivKeyBytes: privateKeyBytes}
-	_, err = NewClient(&settings)
+	_, err = NewClient(&settings, &testEventHandler)
 	if err == nil {
 		t.Fatal("A client with no public key must fail to initialize.")
 	}
 
 	settings = Settings{PubKeyBytes: publicKeyBytes}
-	_, err = NewClient(&settings)
+	_, err = NewClient(&settings, &testEventHandler)
 	if err == nil {
 		t.Fatal("A client with no private key must fail to initialize.")
 	}
 
 	settings = Settings{PrivKeyBytes: privateKeyBytes, PubKeyBytes: publicKeyBytes}
-	_, err = NewClient(&settings)
+	_, err = NewClient(&settings, &testEventHandler)
 	if err == nil {
 		t.Fatal("A client with no peerID must fail to initialize.")
 	}
 
 	settings = Settings{PrivKeyBytes: privateKeyBytes, PubKeyBytes: publicKeyBytes, PeerID: testPeerID}
 	settings.LoadKeys()
-	_, err = NewClient(&settings)
+	_, err = NewClient(&settings, &testEventHandler)
 	if err != nil {
 		t.Fatal(err)
 		t.Fatal("A client with a key pair and a peer ID should initialize correctly.")
